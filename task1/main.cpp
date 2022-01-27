89ed9d9e497b78d9efdb2ed0a9e8f086fd7f7716
feldew <feldew@mail.ru>
2022-01-27 10:06:43 +0300

    task1 complete

---
+++ task1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+	int n,m, pos = 1;
+	if (argc == 1)
+		cin >> n >> m;
+	else if (argc == 3)
+	{
+		n = atoi(argv[1]);
+		m = atoi(argv[2]);
+	}
+	else
+		return (0);
+	do {
+		cout << pos;
+		pos = (pos + m - 1) % n;
+		if (pos == 0)
+			pos = n;
+	}
+	while (pos != 1);
+	cout << endl;
+}
